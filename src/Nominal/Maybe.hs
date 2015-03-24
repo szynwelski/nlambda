887bc92b5dc57ed80c28e8b6b2163d080d3c27b8
Michał Szynwelski <szynwelski@mimuw.edu.pl>
2015-03-24 23:06:43 +0100

    NominalMaybe

---
+++ src/Nominal/Maybe.hs
@@ -0,0 +1,34 @@
+module Nominal.Maybe where
+
+import qualified Data.Maybe as Maybe
+import Formula
+import Nominal.Variants
+import Prelude hiding (filter, map, or)
+
+----------------------------------------------------------------------------------------------------
+-- NominalMaybe
+----------------------------------------------------------------------------------------------------
+
+type NominalMaybe a = Variants (Maybe a)
+
+nothing :: NominalMaybe a
+nothing = variant Nothing
+
+just :: a -> NominalMaybe a
+just = variant . Just
+
+fromJust :: Ord a => NominalMaybe a -> Variants a
+fromJust = map Maybe.fromJust . filter Maybe.isJust
+
+fromMaybe :: Ord a => a -> NominalMaybe a -> Variants a
+fromMaybe dv = map (Maybe.fromMaybe dv)
+
+maybe :: Ord b => b -> (a -> b) -> NominalMaybe a -> Variants b
+maybe dv f = map (Maybe.maybe dv f)
+
+isJust :: NominalMaybe a -> Formula
+isJust = satisfying Maybe.isJust
+
+isNothing :: NominalMaybe a -> Formula
+isNothing = satisfying Maybe.isNothing
+
