b0f1921b6ddce420a9df4e2deddcbfbeed2e1bcd
Michał Szynwelski <szynwelski@mimuw.edu.pl>
2015-03-24 23:06:33 +0100

    NominalEither

---
+++ src/Nominal/Either.hs
@@ -0,0 +1,41 @@
+module Nominal.Either where
+
+import qualified Data.Either.Utils as Utils
+import Formula
+import Nominal.Variants
+import Prelude hiding (filter, map, or)
+
+----------------------------------------------------------------------------------------------------
+-- NominalEither
+----------------------------------------------------------------------------------------------------
+
+type NominalEither a b = Variants (Either a b)
+
+left :: a -> NominalEither a b
+left v = variant (Left v)
+
+right :: b -> NominalEither a b
+right v = variant (Right v)
+
+fromLeft :: Ord a => NominalEither a b -> Variants a
+fromLeft = map Utils.fromLeft . filter isLeft
+    where isLeft (Left _) = True
+          isLeft (Right _) = False
+
+fromRight :: Ord b => NominalEither a b -> Variants b
+fromRight = map Utils.fromRight . filter isRight
+    where isRight (Left _) = False
+          isRight (Right _) = True
+
+fromEither :: Ord a => NominalEither a a -> Variants a
+fromEither = map Utils.fromEither
+
+isLeft :: NominalEither a b -> Formula
+isLeft = satisfying isLeft
+    where isLeft (Left _) = True
+          isLeft (Right _) = False
+
+isRight :: NominalEither a b -> Formula
+isRight = satisfying isRight
+    where isRight (Left _) = False
+          isRight (Right _) = True
