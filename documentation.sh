35f718b244b6e42baed8b7d7a0bd5a8a7ae3de4c
Michał Szynwelski <szynwelski@mimuw.edu.pl>
2016-01-18 00:52:56 +0100

    Version 1.0

--- documentation.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cabal haddock --hyperlink-source
-sed -i 's/file\:\/\/\/usr\/local\/haskell\/ghc\-7\.8\.3\-x86_64\/share\/doc\/ghc\/html\/libraries\/\(base\-4\.7\.0\.1\)/http:\/\/hackage.haskell.org\/package\/\1\/docs/g' dist/doc/html/NLambda/NLambda.html
-sed -i 's/file\:\/\/\/usr\/local\/haskell\/ghc\-7\.8\.3\-x86_64\/share\/doc\/ghc\/html\/libraries\/\(containers\-0\.5\.5\.1\)/http:\/\/hackage.haskell.org\/package\/\1\/docs/g' dist/doc/html/NLambda/NLambda.html
-sed -i 's/file\:\/\/\/usr\/local\/haskell\/ghc\-7\.8\.3\-x86_64\/lib\/\(mtl\-2\.1\.3\.1\)\/doc\/html/http:\/\/hackage.haskell.org\/package\/\1\/docs/g' dist/doc/html/NLambda/NLambda.html
-sed -i 's/Nominal.html/NLambda.html/g' dist/doc/html/NLambda/NLambda.html
