6385f629041b70f08ec9165e1e3bd0678e4aee5f
Michał Szynwelski <szynwelski@mimuw.edu.pl>
2016-10-27 17:55:52 +0200

    NLambda export list fixed

--- src/NLambda.hs
+++ src/NLambda.hs
@@ -7,40 +7,40 @@ Stability:      experimental
 
 Module supports computations over infinite structures using logical formulas and SMT solving.
 -}
-module NLambda where
+module NLambda (module Export) where
 
 #if TOTAL_ORDER
-import Nominal.Atoms as NLambda
+import Nominal.Atoms as Export
 #else
-import Nominal.Atoms as NLambda hiding (lt, le, gt, ge)
+import Nominal.Atoms as Export hiding (lt, le, gt, ge)
 #endif
-import Nominal.Atoms.Signature as NLambda (Constant)
-import Nominal.Atoms.Space as NLambda
-import Nominal.Automaton.Base as NLambda
-import Nominal.Automaton.Deterministic as NLambda
-import Nominal.Automaton.Nondeterministic as NLambda
-import Nominal.Conditional as NLambda
-import Nominal.Contextual as NLambda
-import Nominal.Either as NLambda
+import Nominal.Atoms.Signature as Export (Constant)
+import Nominal.Atoms.Space as Export
+import Nominal.Automaton.Base as Export
+import Nominal.Automaton.Deterministic as Export
+import Nominal.Automaton.Nondeterministic as Export
+import Nominal.Conditional as Export
+import Nominal.Contextual as Export
+import Nominal.Either as Export
 #if TOTAL_ORDER
-import Nominal.Formula as NLambda
+import Nominal.Formula as Export
 #else
-import Nominal.Formula as NLambda hiding (lessThan, lessEquals, greaterThan, greaterEquals)
+import Nominal.Formula as Export hiding (lessThan, lessEquals, greaterThan, greaterEquals)
 #endif
 #if TOTAL_ORDER
-import Nominal.Graph as NLambda
+import Nominal.Graph as Export
 #else
-import Nominal.Graph as NLambda hiding (monotonicGraph)
+import Nominal.Graph as Export hiding (monotonicGraph)
 #endif
-import Nominal.Maybe as NLambda
-import Nominal.Orbit as NLambda
+import Nominal.Maybe as Export
+import Nominal.Orbit as Export
 #if TOTAL_ORDER
-import Nominal.Set as NLambda
+import Nominal.Set as Export
 #else
-import Nominal.Set as NLambda hiding (range, openRange, isLowerBound, hasLowerBound, isUpperBound, hasUpperBound, isMinimum, hasMinimum, isMaximum, hasMaximum, isInfimum, isSupremum, isConnected, isOpen, isClosed, isCompact)
+import Nominal.Set as Export hiding (range, openRange, isLowerBound, hasLowerBound, isUpperBound, hasUpperBound, isMinimum, hasMinimum, isMaximum, hasMaximum, isInfimum, isSupremum, isConnected, isOpen, isClosed, isCompact)
 #endif
-import Nominal.Type as NLambda (BareNominalType(..), NominalType(..), Scope, MapVarFun, FoldVarFun, neq)
-import Nominal.Variable as NLambda (Variable, variable)
-import Nominal.Variants as NLambda (Variants, variant, fromVariant, iteV)
+import Nominal.Type as Export (BareNominalType(..), NominalType(..), Scope, MapVarFun, FoldVarFun, neq)
+import Nominal.Variable as Export (Variable, variable)
+import Nominal.Variants as Export (Variants, variant, fromVariant, iteV)
 import Prelude hiding (or, and, not, sum, map, filter, maybe)
 
