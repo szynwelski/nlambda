fa9ab31c0c9b3a3d76f63fbb38ac70fba82c50b7
Michał Szynwelski <szynwelski@mimuw.edu.pl>
2018-04-27 20:33:18 +0200

    Issue #2 - Module Meta - incomplete implementation.

--- src/meta/Meta.hs
+++ src/meta/Meta.hs
@@ -1,13 +1,40 @@
 module Meta where
 
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import Data.Map (Map)
+import Data.Set (Set)
+
+------------------------------------------------------------------------------------------
+
 data WithMeta a = WithMeta {value :: a, meta :: Meta} deriving (Show, Eq)
 
-type Meta = Int
-type IdsMap = Int
-type Union = (Meta, [IdsMap])
+type Identifier = Int
+type IdMap = Map Identifier Identifier
+type IdPairSet = Set (Identifier, Identifier)
+type Meta = (IdMap, IdPairSet)
+type Union = (Meta, [IdMap])
+
+-- TODO
+replaceVariablesIds :: IdMap -> a -> a
+replaceVariablesIds = undefined
+
+splitMapping :: IdPairSet -> IdMap -> (IdMap, IdMap)
+splitMapping s m = if Set.null s then (Map.empty, m) else Map.partitionWithKey (\k v -> Set.member (k, v) s) m
+
+renameMaps :: Meta -> Meta -> (Meta, IdMap, IdMap)
+renameMaps (m1, s1) (m2, s2) = ((Map.union m1b m2c, Set.unions [s1, s2, Set.fromList $ Map.assocs m2d]), m1a, Map.union m2a m2d)
+    where (m1a, m1b) = splitMapping s2 m1
+          (m2a, m2b) = splitMapping s1 m2
+          (m2c, m2d) = if Map.null m1b then (m2b, Map.empty) else Map.partitionWithKey (\k v -> v == Map.findWithDefault v k m1b) m2b
+
+------------------------------------------------------------------------------------------
+
+metaFromMap :: IdMap -> Meta
+metaFromMap map = (map, Set.empty)
 
 emptyMeta :: Meta
-emptyMeta = 1
+emptyMeta = (Map.empty, Set.empty)
 
 empty :: a -> WithMeta a
 empty x = WithMeta x emptyMeta
@@ -16,13 +43,18 @@ create :: a -> Meta -> WithMeta a
 create x m = WithMeta x m
 
 union :: [Meta] -> Union
-union ms = (0, [])
+union [] = (emptyMeta, [])
+union [m] = (m, [Map.empty])
+union (m:ms) = let (m', idMap:idMaps) = union ms
+                   (m'', idMap1, idMap2) = renameMaps m m'
+               in (m'', idMap1 : (Map.union idMap2 idMap) : idMaps)
 
 getMeta :: Union -> Meta
 getMeta = fst
 
-rename :: Union -> Int -> a -> a -- Int mówi, którą mapę z [IdsMap] wziąć do zmiany nazwy
-rename _ _ = id
+rename :: Union -> Int -> a -> a
+rename (_, idMaps) n x = let idMap = idMaps !! n
+                         in if null idMap then x else x
 
 emptyList :: [a]
 emptyList = []
